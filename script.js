cf9af6bd7f98ad94911dd427545f8764fb1a6995
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-05 20:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let input = document.getElementById("fname");
+input.addEventListener("focusout", myBlurFunction);
+
+function myBlurFunction() {
+    let currval = input.value;
+    let modifiedVal = currval.trim().toUpperCase();
+    input.value = modifiedVal;
+}
